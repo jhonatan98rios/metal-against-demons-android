a3f2b2548f6c1f78e5d57ce1c937aee0b0af72ea
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2024-05-11 04:35:13 -0300

    adding compose

--- android/src/com/teixeirarios/mad/MenuActivity.kt
+++ android/src/com/teixeirarios/mad/MenuActivity.kt
@@ -2,22 +2,41 @@ package com.teixeirarios.mad
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        setContent {
+            MenuScreen(onButtonClick = {
+                val intent = Intent(this, AndroidLauncher::class.java)
+                startActivity(intent)
+            })
+        }
+    }
+}
 
-        // Encontre o botão pelo ID
-        val startGameButton: Button = findViewById(R.id.startGameButton)
-
-        // Configurar um listener de clique para o botão
-        startGameButton.setOnClickListener {
-            // Iniciar a Activity do jogo
-            val intent = Intent(this, AndroidLauncher::class.java)
-            startActivity(intent)
+@Composable
+fun MenuScreen(onButtonClick: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+        verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center
+    ) {
+        Text(text = "Bem-vindo ao Menu", modifier = Modifier.padding(bottom = 16.dp))
+        Button(onClick = { onButtonClick() }) {
+            Text(text = "Iniciar Jogo")
         }
     }
 }
